081764151d43290221c8236dc5f4baa8ad86c763
benja <bpham1618@gmail.com>
2023-12-02 20:03:04 -0700

    error handling for adding songs to playlists that don't belong to the user

--- app.py
+++ app.py
@@ -3,20 +3,13 @@ import psycopg2
 from psycopg2 import Error
 import random
 
-# change these values according to your local machine
-user = 'postgres'
-password = '1234'
-port = '5432'           # for Ben it's 5432, for Philip it's 5433
-database = 'phase2'     # may be different for you depending on what database you are using on your local mahcine
-                        # for me i had to create another database different from postgres which is default
-                        # then create all the tables and insert the data
 try:
     # Connect to an existing database
-    connection = psycopg2.connect(user=user,
-                                  password=password,
+    connection = psycopg2.connect(user="postgres",
+                                  password="1234",
                                   host="localhost",
-                                  port=port,
-                                  database=database)
+                                  port="5432",
+                                  database="phase2")
 
     # Create a cursor to perform database operations
     cursor = connection.cursor()
@@ -37,15 +30,14 @@ finally:
         connection.close()
         print("PostgreSQL connection is closed")
 
-#make changes
 def get_db_connection():
     conn = psycopg2.connect(
-        user=user,
-        password=password,
+        user="postgres",
+        password="1234",
         host="localhost",
-        port=port,
-        database=database
-    )
+        port="5432",        # for Ben it's 5432, for Philip it's 5433
+        database="phase2"   # may be different for you depending on what database you are using on your local mahcine
+    )                       # for me i had to create another database different from postgres which is default
     return conn
 
 #-----------this is the part to test if you can connect to postgresql---------------
@@ -54,8 +46,7 @@ def get_db_connection():
 
 
 # testing to see how well using a global user variable works
-username = None
-password = None
+user = None
 
 app = Flask(__name__, template_folder='templates')
 
@@ -65,8 +56,8 @@ def index():
 
 @app.route('/user-login', methods=['GET', 'POST'])
 def user_login():
-    global username
-    global password
+    global user
+    
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -75,8 +66,7 @@ def user_login():
         cur.execute("SELECT * FROM users WHERE username = %s AND password = %s",
                     (username, password))
         user = cur.fetchone()   # user is a tuple ie (0, 'user0', 'pass0')
-        username = user[1]  # 1 is the user name in the tuple
-        password = user[2]  # this grabs the password within the tuple
+        
         cur.close()
         conn.close()
         if user:
@@ -112,8 +102,8 @@ def create_account():
 # this is for the general use of navigating back and forth from playlist page once user has already logged in
 @app.route('/playlist', methods=['GET', 'POST'])
 def playlist():
-    global username
-    user_id = username  # Replace with the actual logged-in user's ID
+    global user
+    user_id = user[0]
     print(user_id)
     # playlist_name = request.form['playlist_name']
 
@@ -135,7 +125,7 @@ def playlist():
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    global username
+    global user
     songs_list = ()
     
     # display all songs upon loading up the page
@@ -155,30 +145,39 @@ def search():
         song = request.form['song']
         playlist = request.form['playlist']
         
-        # now add to database specifying user, song, and playlist
+        # connect to database
         conn = get_db_connection()
         cur = conn.cursor()
 
-        # first we want to add to the song table the song that was chosen that references the playlist name
-        # randomly generate the other columns - song_id, num_listens, and album_id
-        song_id = random.randint(11, 10000)
-        num_listens = random.randint(1, 1000000)
-        album_id = random.randint(11, 100000)
-        
-        cur.execute("INSERT INTO song (song_id, song_name, num_listens, album_id, playlist_name) VALUES (%s,%s,%s,%s,%s)", 
-                    (song_id, song, num_listens, album_id, playlist))
+        # we want to first check if what the user entered as the playlist exists
+        cur.execute("SELECT playlist_name FROM playlist WHERE user_id = %s AND playlist_name = %s", 
+                    (user[0], playlist))
+        temp = cur.fetchall()
+        # print(temp)
 
-        conn.commit()
-        cur.close()
-        conn.close()
+        if not temp:
+            error = (f"{playlist} doesn't exist")
+            return render_template('search.html', songs_list=songs_list, error=error)
+        else:
+            # now we want to actually add the song to the song table that references the playlist
+            # randomly generate the other columns - song_id, num_listens, and album_id
+            song_id = random.randint(11, 10000)
+            num_listens = random.randint(1, 1000000)
+            album_id = random.randint(11, 100000)
+            
+            cur.execute("INSERT INTO song (song_id, song_name, num_listens, album_id, playlist_name) VALUES (%s,%s,%s,%s,%s)", 
+                        (song_id, song, num_listens, album_id, playlist))
+
+            conn.commit()
+            cur.close()
+            conn.close()
     
     return render_template('search.html', songs_list=songs_list)
 
 @app.route('/my_account', methods=['GET', 'POST'])
 def my_account():
-    global username
-    global password
-    return render_template('my_account.html', username=username, password=password) # render username and password upon loeading this page
+    global user
+    return render_template('my_account.html', username=user[1], password=user[2]) # render username and password upon loeading this page
 
 
 if __name__ == "__main__":
